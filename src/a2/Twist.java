f8dd9b452b2ca266a545501f377228c21f70d7d5
Sarius <sarius@erebos.fritz.box>
2018-11-09 11:25:04 +0100

    started to write Twist.java

--- src/a2/Twist.java
+++ src/a2/Twist.java
@@ -1,10 +1,32 @@
 package a2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Twist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// when there are no parameters, the program hasn't anything to do
+		if (args.length == 0) {
+			System.out.println("Please run program with a file as parameter.");
+		} else {
+			// program can be run with multiple files as parameters
+			for (int i = 0; i < args.length; i++) {
+				try {
+					File sourceFile = new File(args[i]);
+					Scanner sc = new Scanner(sourceFile);
+					
+					while(sc.hasNext(Pattern.compile("\w"))) {
+						
+					}
+					
+					sc.close();
+				} catch (FileNotFoundException e) {
+					
+				}
+			}
+		}
 	}
-
 }
